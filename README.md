e9289371d353cebd8d0a9e155f30d26c3e9041f8
vidhi09-tech <vidhi2556@gmail.com>
2020-07-13 09:30:16 +0530

    blinking of a led

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # led_patterns_arduion
-as an electronic student, arduino plays a key role in their lives.. 
+
+As an electronic student, arduino plays a key role in their lives..
+Arduino is an open-source electronics prototyping platform based on flexible, easy-to-use hardware and software.
+It's intended for artists, designers, hobbyists, and anyone interested in creating interactive objects or environments.
+The microcontroller on the board is programmed using the Arduino programming language and Arduino Development Environment.
+
+I have write the program for 5 led patterns and even uploaded the circuit diagram with the output.. It will make others easy to understand the program..
---
+++ pattern2/led_pattern2.ino
@@ -0,0 +1,45 @@
+//pin declartaion
+//pin mode declaration
+//pin data declaration
+int led1 = 12;
+int led2 = 11;
+int led3 = 9;
+int led4 = 6;
+int led5 = 4;
+int led6 = 2;
+
+void setup()
+{
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+    pinMode(led4, OUTPUT);
+    pinMode(led5, OUTPUT);
+    pinMode(led6, OUTPUT);
+}
+void loop()
+{
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    delay(1000);
+
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    delay(1000);
+
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, LOW);
+    delay(1000);
+
+    digitalWrite(led2, HIGH);
+    digitalWrite(led1, LOW);
+    delay(1000);
+
+    digitalWrite(led4, HIGH);
+    digitalWrite(led3, LOW);
+    delay(1000);
+
+    digitalWrite(led6, HIGH);
+    digitalWrite(led5, LOW);
+    delay(1000);
+}
